8a4db9bd3b7d602d42951535822697eee5e9de02
unknown <collid@GBWS1096.daiwaeurope.local>
2012-02-08 13:51:07 +0000

    Added slightly better IDataErrorInfo implementation

--- Examples/DataErrorInfo/DataErrorInfo/DataErrorInfoAmender.cs
+++ Examples/DataErrorInfo/DataErrorInfo/DataErrorInfoAmender.cs
@@ -1,22 +1,54 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace DataErrorInfo
 {
-    [CLSCompliant(false)]
     public static class DataErrorInfoAmender<T> where T : class
     {
-        public static string ValidateProperty(IDataErrorInfo instance, string propertyName)
+        private static readonly IDictionary<string, ValidationAttribute[]> PropertyToValidatorsDictionary = new Dictionary<string, ValidationAttribute[]>();
+        private static readonly IDictionary<string, Func<T, object>> PropertyToGetterDictionary = new Dictionary<string, Func<T, object>>();
+
+        static DataErrorInfoAmender()
         {
-            var prop = instance.GetType().GetProperty(propertyName);
-            var validators = prop.GetCustomAttributes(typeof(ValidationAttribute), true).Cast<ValidationAttribute>();
-            return (from v in validators where !v.IsValid(prop.GetValue(instance, null)) select v.FormatErrorMessage(propertyName)).FirstOrDefault();
+            var amendedType = typeof(T);
+            foreach (var propertyInfo in amendedType.GetProperties())
+            {
+                var validators = propertyInfo.GetCustomAttributes(typeof(ValidationAttribute), true) as ValidationAttribute[];
+                if (validators == null || validators.Length <= 0) continue;
+
+                PropertyToValidatorsDictionary[propertyInfo.Name] = validators;
+
+                var indexParam = propertyInfo.GetIndexParameters();
+                if (indexParam.Length != 0) continue;
+                var expressionParameter = Expression.Parameter(amendedType, "t");
+                var cast = Expression.TypeAs(Expression.Property(expressionParameter, propertyInfo), typeof(object));
+                PropertyToGetterDictionary[propertyInfo.Name] = Expression.Lambda(cast, expressionParameter).Compile() as Func<T, object>;
+
+                //indexer case
+                //{ 
+                //    var accessor = propertyInfo.GetGetMethod(); 
+                //    var instance = Expression.Parameter(typeToProxy, "t"); 
+                //    var index = Expression.Parameter(indexParam[0].ParameterType, "i"); 
+                //    var exp = Expression.Call(instance, accessor, index); 
+                //    var cast = Expression.TypeAs(exp, typeof (object)); 
+                //    getter = Expression.Lambda(cast, instance, index).Compile(); 
+                //}
+            }
         }
 
-        public static string GetError(IDataErrorInfo instance, string propertyname)
+        public static string ValidateProperty(T instance, string propertyName)
         {
+            ValidationAttribute[] validators;
+            if (PropertyToValidatorsDictionary.TryGetValue(propertyName, out validators))
+            {
+                var getter = PropertyToGetterDictionary[propertyName];
+                var value = getter(instance);
+                var errors = validators.Where(v => !v.IsValid(value)).Select(v => v.FormatErrorMessage(propertyName)).ToArray();
+                return string.Join(Environment.NewLine, errors);
+            }
             return null;
         }
     }
--- Examples/DataErrorInfo/DataErrorInfo/DataErrorInfoAmendment.cs
+++ Examples/DataErrorInfo/DataErrorInfo/DataErrorInfoAmendment.cs
@@ -4,13 +4,12 @@ using Afterthought;
 
 namespace DataErrorInfo
 {
-    [CLSCompliant(false)]
-    public class DataErrorInfoAmendment<T> : Amendment<T, IDataErrorInfo> where T : class
+    public class DataErrorInfoAmendment<T> : Amendment<T, T> where T : class
     {
 		public DataErrorInfoAmendment()
 		{
 		    Implement<IDataErrorInfo>(
-                Properties.Add<string>("Error", DataErrorInfoAmender<T>.GetError),
+                Properties.Add<string>("Error", (instance, name) => null),
                 Methods.AddPropertyIndexerGetter<string, string>(DataErrorInfoAmender<T>.ValidateProperty)
 		    );
 		}
