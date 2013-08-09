6f4980b5148459f6f44fdfcd3224c6c735d48e56
Aren Blondahl <rean1985@gmail.com>
2013-08-09 01:44:18 -0600

    Fixed a traversal bug that lead to nested types not being amended unless their parent class was amended too.

---
+++ Afterthought.UnitTest.Target/TestNestedAmendment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Afterthought.UnitTest.Target
+{
+    public class TestNestedAmendment<T> : Amendment<T,T>
+        where T : Nested.Example
+    {
+        public TestNestedAmendment()
+        {
+            Methods
+                .Named("Add")
+                .WithParams<int, int>()
+                .Implement((T instance, int a, int b) =>
+                               {
+                                   instance.Result = a + b;
+                                   return a + b;
+                               });
+        }
+    }
+}
