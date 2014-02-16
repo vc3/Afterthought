e95328b34fa9a6a15b73fc2ebcd313d11ae27052
Dmitry Kononchuk <kononchukdmitry@gmail.com>
2014-02-16 12:27:07 +0100

    Fix newly added unit test

--- Afterthought.UnitTest/GenericTypeAmendmentTests.cs
+++ Afterthought.UnitTest/GenericTypeAmendmentTests.cs
@@ -14,6 +14,7 @@ namespace Afterthought.UnitTest
             Calculator = new GenericCalculator<int>();
         }
 
+        [TestMethod]
         public void AddAfter()
         {
             Assert.IsFalse(Calculator.MethodExecuted);
