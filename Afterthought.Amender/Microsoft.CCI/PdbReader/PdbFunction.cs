0dd06ef4c365ba1dc8ff4a3583825d9d73f23941
Bobby Dimmick <kr4ster@gmail.com>
2017-03-13 09:18:45 -0400

    Update to support Roslyn metadata

--- Afterthought.Amender/Microsoft.CCI/PdbReader/PdbFunction.cs
+++ Afterthought.Amender/Microsoft.CCI/PdbReader/PdbFunction.cs
@@ -357,6 +357,7 @@ namespace Microsoft.Cci.Pdb {
         case 2: break; // this.ReadForwardedToModuleInfo(bits); break;
         case 3: this.ReadIteratorLocals(bits); break;
         case 4: this.ReadForwardIterator(bits); break;
+		case 5: break;
 		case 6: break;
 		case 7: break;
 		default: throw new PdbDebugException("Unknown custom metadata item kind: {0}", kind);
